918a4395c6777bce0772e0e2b9118a0b7d034561
munnatamton <munnatamton0712@gmail.com>
2024-07-12 19:49:13 +0530

    Updated Navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-black text-white'>
-        <ol className='flex justify-between'>
+    <>
+    <div className='bg-black text-white px-6 py-6 font-medium text-sm'>
+        <ul className='flex space-x-5'>
+            <li><img src="./NetflixLogoSvg.png" alt="netflix-logo"/></li>
             <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Filmler</li>
-            <li>Diziler</li>
-        </ol>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>New & Popular</li>
+            <li>My List</li>
+            <li>Browse by Languages</li>
+        </ul>
     </div>
+    </>
   )
 }
 
