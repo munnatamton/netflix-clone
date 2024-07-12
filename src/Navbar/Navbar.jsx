0c17839b8f9829fb038efafa111fa6322f79a503
Rijas Abdul Razak <rijas5198@gmail.com>
2024-07-12 19:56:53 +0530

    logo load and item center

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <>
     <div className='bg-black text-white px-6 py-6 font-medium text-sm'>
-        <ul className='flex space-x-5'>
-            <li><img src="./NetflixLogoSvg.png" alt="netflix-logo"/></li>
+        <ul className='flex item-center space-x-5'>
+            <li><img src="logos/NetflixLogoSvg.png" className="h-[30px]" alt="netflix-logo"/></li>
             <li>Home</li>
             <li>TV Shows</li>
             <li>Movies</li>
